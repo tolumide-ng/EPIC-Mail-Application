bf255c60f46ebc590a49e0d18f55134b487e7296
Mosinmiloluwa Owoso <mosinmiloluwa.o@gmail.com>
2019-02-26 23:59:23 +0100

    Create reset password page

--- UI/js/app.js
+++ UI/js/app.js
@@ -16,3 +16,23 @@ setTimeout(function(){
   signUpModal.style.visibility = 'hidden';
   }, signupTimeOut);
 /*-------- for sign up modal -----*/
+
+//declare a variable to save time out in milliseconds
+var resetPasswordTimeOut = 5000;
+
+/*-------- for reset password modal -----*/
+//get the id of the modal to be displayed
+var resetPwordModal = document.getElementById('resetPasswordModal');
+
+//call the function to display the sign up modal
+function openResetPasswordModal(){
+  resetPwordModal.style.display = 'block';
+}
+
+//call the function to hide sign up modal after some time
+setTimeout(function(){
+  resetPwordModal.style.visibility = 'hidden';
+  }, resetPasswordTimeOut);
+/*-------- for reset password modal -----*/
+
+/*-----------------------------------------------------*/
