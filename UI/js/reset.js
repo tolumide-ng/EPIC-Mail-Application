6d341068824dc32b2fea221b16fd72ba52445e96
Mosinmiloluwa-Owoso <mosinmiloluwa.o@gmail.com>
2019-03-10 00:46:04 +0100

    modify reset password page

--- UI/js/reset.js
+++ UI/js/reset.js
@@ -1,16 +1,24 @@
 //credit: all modals is gotten from w3schools
 // Get the modal
-var registerModal = document.getElementById('signupModal');
+let registerModal = document.getElementById('signupModal');
 
 // Get the button that opens the modal
-var openReset = document.getElementById("open-reset-modal");
+let openReset = document.getElementById("open-reset-modal");
 
 // Get the <span> element that closes the modal
-var span = document.getElementsByClassName("close")[0];
+let span = document.getElementsByClassName("close")[0];
+let oldpwd = document.getElementById("oldpwd");
+let newpwd = document.getElementById("newpwd");
+let cnewpwd = document.getElementById("cnewpwd");
 
 // When the user clicks on the button, open the modal 
 openReset.onclick = function() {
+  if (oldpwd.value !="" || newpwd.value !="" || cnewpwd.value !=""){
   registerModal.style.display = "block";
+  }
+  else{
+    registerModal.style.display = "none";
+  }
 }
 
 // When the user clicks on <span> (x), close the modal
