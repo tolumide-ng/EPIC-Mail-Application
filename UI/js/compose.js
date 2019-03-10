9cbe62a8b3eb54970a4cc0c475277472e2e61479
Mosinmiloluwa-Owoso <mosinmiloluwa.o@gmail.com>
2019-03-10 01:23:46 +0100

    upload to gh-pages

--- UI/js/compose.js
+++ UI/js/compose.js
@@ -9,13 +9,17 @@ let span = document.getElementsByClassName("close")[0];
 let span1 = document.getElementsByClassName("close1")[0];
 
 let retract = document.getElementById("retract");
+let to = document.getElementById("to");
+let cc = document.getElementById("cc");
 
 let save = document.getElementById("saveModal");
 let saveFeedback = document.getElementById("save-feedback");
 
 // When the user clicks on the button, open the modal 
 openFeedback.onclick = function() {
+  if (to.value !=""){
   feedbakModal.style.display = "block";
+  }
 }
 
 // When the user clicks on <span> (x), close the modal
--- UI/js/signin.js
+++ UI/js/signin.js
@@ -1,16 +1,29 @@
 //credit: w3schools
 // Get the modal
-var registerModal = document.getElementById('signupModal');
+const registerModal = document.getElementById('signupModal');
 
 // Get the button that opens the modal
-var openSignup = document.getElementById("open-signup-modal");
+const openSignup = document.getElementById("open-signup-modal");
 
 // Get the <span> element that closes the modal
-var span = document.getElementsByClassName("close")[0];
+const span = document.getElementsByClassName("close")[0];
+
+let fname = document.getElementById("fname"); 
+let lname = document.getElementById("lname");
+let uname = document.getElementById("uname");
+let pwd = document.getElementById("pwd");
+let cpwd = document.getElementById("cpwd");
 
 // When the user clicks on the button, open the modal 
+
 openSignup.onclick = function() {
+  if(fname.value != "" || lname.value != "" || uname.value != "" || pwd.value != "" || cpwd.value != ""){
   registerModal.style.display = "block";
+  console.log(fname);
+  }
+  else{
+    registerModal.style.display = "none";
+  }
 }
 
 // When the user clicks on <span> (x), close the modal
